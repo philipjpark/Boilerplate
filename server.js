d5140bd234bfc1a84fafa507b1744fa030a6c807
philipjpark <66290921+philipjpark@users.noreply.github.com>
2020-08-24 10:50:48 -0400

    Middleware being hooked in

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
 const port = 4000;
 
-app.listen(port, () => {
-    console.log('Server Started on Port ${port}');
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-});
 
 app.get('/api/students', (req, res) => {
-const students = [
-    { id: 1, firstName: 'Captain', lastName: 'Fancy'},
-    {id: 2, firstName: 'John', lastName: 'buttercup'},
-    { id: 3, firstName: 'Dusty', lastName: 'Trail'},
-];
-res.json(students);  
-
+    const students = [
+        { id: 1, firstName: 'Captain', lastName: 'Fancy'},
+        { id: 2, firstName: 'John', lastName: 'Buttercup'},
+        { id: 3, firstName: 'Dusty', lastName: 'Trail'},
+    ];
+    res.json(students);
+})
+app.listen(port, () => {
+    console.log(`Server Started on Port: ${port}`);
 });
